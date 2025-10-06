f67a14bb29d51341dc57ef2629a2fb071e48d208
SITHU NYEIN <sithunyein.official@hotmail.com>
2025-10-06 12:37:16 +0630

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase, sendMagicLink } from './lib/supabaseClient';
+import { supabase } from './lib/supabaseClient';
 import Auth from './components/Auth';
 import Account from './components/Account';
 
@@ -7,21 +7,24 @@ const App = () => {
   const [session, setSession] = useState(null);
 
   useEffect(() => {
-    // Get current session on page load
-    const session = supabase.auth.getSession()?.data.session;
-    setSession(session);
-
-    // Listen for login/logout events
-    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
       setSession(session);
     });
 
-    return () => subscription.unsubscribe();
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
   }, []);
 
   return (
     <div className="container" style={{ padding: '50px 0 100px 0' }}>
-      {!session ? <Auth sendMagicLink={sendMagicLink} /> : <Account key={session.user.id} session={session} />}
+      {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
     </div>
   );
 };
